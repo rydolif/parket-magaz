d03550bd695e8c5865a0a6507ffade3bfd7be098
Mykola Polishchuk <rudolifrudolif@gmail.com>
2019-11-07 22:59:02 +0200

    cart + hero commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,33 @@
 $(function() {
 
 //------------------------------гамбургер-----------------------------
+  var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+//------------------------------гамбургер-----------------------------
   $('.hamburger').click(function() {
     $(this).toggleClass('hamburger--active');
     $('nav').toggleClass('nav--active');
     $('header').toggleClass('header--menu');
   });
 
+//---------------------------js-----------------------
+  $('.tabs__wrap').hide();
+  $('.tabs__wrap:first').show();
+  $('.tabs ul a:first').addClass('tabs__link--active');
+   $('.tabs ul a').click(function(event){
+    event.preventDefault();
+    $('.tabs ul a').removeClass('tabs__link--active');
+    $(this).addClass('tabs__link--active');
+    $('.tabs__wrap').hide();
+     var selectTab = $(this).attr('href');
+    $(selectTab).fadeIn();
+  });
 
   
 });
